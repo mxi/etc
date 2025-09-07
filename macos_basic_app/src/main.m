e1a298f20b457e77e79e57a498bb11e1092e0c2a
Maxim Kasyanenko <mkasyane@gmail.com>
2025-09-07 11:57:15 -0700

    basic window

--- macos_basic_app/src/main.m
+++ macos_basic_app/src/main.m
@@ -1,10 +1,37 @@
+#import <AppKit/AppKit.h>
 #import <Foundation/Foundation.h>
+
+@interface AppDelegate : NSObject<NSApplicationDelegate>
+@end
+
+@implementation AppDelegate
+{
+    NSWindow *_window;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    NSRect frame = NSMakeRect(0, 0, 960, 640);
+
+    _window = [[NSWindow alloc] initWithContentRect:frame 
+                                          styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskResizable | NSWindowStyleMaskClosable
+                                            backing:NSBackingStoreBuffered
+                                              defer:NO];
+
+    [_window setTitle:@"MacOS Application Basic"];
+    [_window center];
+    [_window orderFrontRegardless];
+}
+
+@end
 
 int main(void)
 {
     @autoreleasepool
     {
-        NSLog(@"Hello world!");
+        [NSApplication sharedApplication];
+        [NSApp setDelegate:[AppDelegate alloc]];
+        [NSApp run];
     }
     return 0;
 }
